7619f0e8a40a5109512a8f6a3f61ecf34cc21d44
Prakash Surya <prakash.surya@delphix.com>
2020-04-22 10:25:55 -0700

    asdf

--- .github/scripts/sync-with-master.sh
+++ .github/scripts/sync-with-master.sh
@@ -1,14 +1,9 @@
 #!/bin/bash -eux
 
-echo hi
-
 function die() {
 	echo "$(basename "$0"): $*" >&2
 	exit 1
 }
-
-#HEAD=$(git symbolic-ref --short HEAD) || die "HEAD is not a symbolic ref"
-#[[ -n "$HEAD" ]] || die "unable to determine symbolic ref for HEAD"
 
 [[ -z "${GITHUB_REF}" ]] && die "GITHUB_REF is empty"
 
@@ -19,13 +14,24 @@ function die() {
 git show-ref --heads "${GITHUB_REF}" &>/dev/null ||
 	die "GITHUB_REF (${GITHUB_REF}) is not a branch"
 
-git fetch --prune --unshallow
-
+#
+# In order to perform the merge below, we need to have the following git
+# configuration to be set.
+#
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-git merge origin/master
-git push
+#
+# Additionally, in order to perform the merge below, we need the full
+# history for the repository, so the merge can operate on that history.
+#
+git fetch --unshallow
 
-echo hi
-echo ho
+#
+# Now we can actually attempt the merge with the master branch.
+#
+git merge origin/master
+
+BRANCH=$(git symbolic-ref --short HEAD) || die "HEAD is not a symbolic ref"
+[[ -n "${BRANCH}" ]] || die "unable to determine symbolic ref for HEAD"
+git push origin "HEAD:sync-with-master/${BRANCH}"
