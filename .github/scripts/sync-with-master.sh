14f321f06411b80f094374b636993c3cc4c9b7ec
Prakash Surya <prakash.surya@delphix.com>
2020-04-22 09:38:17 -0700

    asdf

--- .github/scripts/sync-with-master.sh
+++ .github/scripts/sync-with-master.sh
@@ -1,12 +1,20 @@
 #!/bin/bash -eux
 
-[[ -z "${GITHUB_REF}" ]] && exit 1
+function die() {
+	echo "$(basename "$0"): $*" >&2
+	exit 1
+}
+
+[[ -z "${GITHUB_REF}" ]] && die "GITHUB_REF is empty"
 
 #
 # We only want to operate on branches, so we verify the passed in ref is
 # a branch (e.g. rather than a tag) here.
 #
-git show-ref --heads "${GITHUB_REF}" &>/dev/null || exit 0
+git show-ref --heads "${GITHUB_REF}" &>/dev/null ||
+	die "GITHUB_REF (${GITHUB_REF}) is not a branch"
+
+[[ "${GITHUB_REF}"
 
 git branch -v -a
 
