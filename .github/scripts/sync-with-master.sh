04d498970daa8c7ce59092d22ff328f923e902d9
Prakash Surya <prakash.surya@delphix.com>
2020-04-22 14:20:41 -0700

    fixup: use scripts from master branch

--- .github/scripts/sync-with-master.sh
+++ .github/scripts/sync-with-master.sh
@@ -1,38 +1,28 @@
 #!/bin/bash -eux
 
 #
+# This variable must be passed into this script.
+#
+[[ -n "${BRANCH}" ]] || exit 1
+
+#
 # We need these config parameters set in order to do the git-merge.
 #
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 #
-# We obtain the original name for the branch we'll be syncing with
-# master. We gather this information here, since we'll change the name
-# of the local branch later, and we'd like to use the original branch
-# name when opening the pull request.
-#
-BRANCH=$(git symbolic-ref --short HEAD)
-[[ -n "${BRANCH}" ]] || exit 1
-
-#
 # We need the full git repository history in order to do the git-merge.
 #
 git fetch --unshallow
-git merge origin/master
-
-#
-# We modify the local branch name and upstream tracking branch such that
-# the correct operations occur when pushing and opening the pull request.
-#
-git branch -M "sync-with-master/${BRANCH}"
-git branch --set-upstream-to "origin/${BRANCH}"
 
 #
 # In order to open a pull request, we need to push to a remote branch.
 # To avoid conflicting with existing remote branches, we use branches
-# within the "sync-with-master" namespace on the remote repository.
+# within the "sync-with-master" namespace.
 #
+git checkout -b "sync-with-master/${BRANCH}" "origin/${BRANCH}"
+git merge -Xtheirs origin/master
 git push -f origin "sync-with-master/${BRANCH}"
 
 #
